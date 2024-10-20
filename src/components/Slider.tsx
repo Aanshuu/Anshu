576d87f3aa23af1f0079d74d91caaa5e3bba37a8
Aanshuu <e21cseu0085@bennett.edu.in>
2024-10-20 22:04:13 +0530

    Refactor Slider component to update image URLs and background colors

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -15,10 +15,10 @@ const HomePage: FC = () => {
   });
 
   useEffect(() => {
-    const streakDarkThemeImage = `https://streak-stats.demolab.com/?user=aanshuu&theme=default&hide_border=true`;
-    const streakLightThemeImage = `https://streak-stats.demolab.com/?user=aanshuu&theme=highcontrast&hide_border=true`;
-    const languageDarkThemeImage = `https://github-readme-stats.vercel.app/api/top-langs/?username=aanshuu&layout=compact&bg_color=00000000&text_color=000000&hide_border=true`
-    const languageLightThemeImage = `https://github-readme-stats.vercel.app/api/top-langs/?username=aanshuu&layout=compact&bg_color=00000000&text_color=ffffff&hide_border=true`
+    const streakDarkThemeImage = `https://streak-stats.demolab.com/?user=aanshuu&background=fecfc5&hide_border=true&stroke=000000`;
+    const streakLightThemeImage = `https://streak-stats.demolab.com/?user=aanshuu&theme=highcontrast&hide_border=true&stroke=fecfc5&currStreakNum=fecfc5&sideNums=fecfc5&currStreakLabel=fecfc5&sideLabels=fecfc5`;
+    const languageDarkThemeImage = `https://github-readme-stats.vercel.app/api/top-langs/?username=aanshuu&layout=compact&bg_color=00000000&text_color=000000&hide_border=true&title_color=000000`
+    const languageLightThemeImage = `https://github-readme-stats.vercel.app/api/top-langs/?username=aanshuu&layout=compact&bg_color=00000000&text_color=fecfc5&hide_border=true&title_color=fecfc5`
 
     // Preload both images once
     const preloadImages = () => {
@@ -43,29 +43,29 @@ const HomePage: FC = () => {
   }, [theme]);
 
   const items = [
-    <div key="1" className="h-full w-full flex items-center justify-center bg-black dark:bg-white">
+    <div key="1" className="h-full w-full flex items-center justify-center bg-primary dark:bg-black">
       <RevealText/>
     </div>,
-    <div key="2" className="h-full w-full flex flex-col items-center justify-center bg-black dark:bg-white">
+    <div key="2" className="h-full w-full flex flex-col items-center justify-center bg-primary dark:bg-black">
       {/* Language Stats */}
       <img
-        src={theme === "dark" ? githubStats.languageDark : githubStats.languageLight}
+        src={theme === "dark" ? githubStats.languageLight : githubStats.languageDark}
         alt="Language Stats"
         className="w-full max-w-lg overflow-hidden"
         loading="lazy" // Lazy load for performance
       />
     </div>,
-    <div key="3" className="h-full w-full flex flex-col items-center justify-center bg-black dark:bg-white">
+    <div key="3" className="h-full w-full flex flex-col items-center justify-center bg-primary dark:bg-black">
       {/* GitHub Stats: Switch between preloaded dark and light theme images */}
       <img
-        src={theme === "dark" ? githubStats.streakDark : githubStats.streakLight}
+        src={theme === "dark" ? githubStats.streakLight : githubStats.streakDark}
         alt="GitHub Stats"
         className="w-full max-w-md"
         loading="lazy" // Lazy load for performance
       />
     </div>,
-    <div key="4" className="h-full w-full flex items-center justify-center bg-black dark:bg-white">
-      <h1 className="text-3xl font-bold text-black">More Content</h1>
+    <div key="4" className="h-full w-full flex items-center justify-center bg-primary dark:bg-black">
+      <h1 className="text-3xl font-bold text-black dark:text-[#fecfc5]">More Content</h1>
     </div>,
   ];
 
