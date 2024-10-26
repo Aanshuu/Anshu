b72064245fbded590a36747234d10dedbbc0298f
Aanshuu <e21cseu0085@bennett.edu.in>
2024-10-26 08:58:30 +0530

    minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from "@/components/theme/ThemeContext";
 import Navbar from "@/components/common/Navbar";
 import Diagram from "@/components/skills/Diagram";
 import Flow from "@/components/skills/TechStackFlow";
-import { Switch } from "@/components/ui/switch";
+// import { Switch } from "@/components/ui/switch";
 // import { CardHoverEffectDemo } from "@/components/info/Projects";
 // import { NavigationButtons } from "@/components/Tabs";
 import { ProjectsExperienceDemo } from "@/components/Tabs";
@@ -65,38 +65,6 @@ export default function Home() {
           </div>
         </>
       ) : null}
-
-      {/* <div>
-        <div className="relative isolate">
-          <div
-            aria-hidden="true"
-            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          >
-            <div
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-              className="relative left-[calc(50%-11rem)] aspect-[1155/508] w-[56.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="relative isolate">
-        <div
-          aria-hidden="true"
-          className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className="relative right-0 aspect-[1155/508] w-[56.125rem] translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:w-[72.1875rem]"
-          />
-        </div>
-      </div> */}
 
       <div className="relative h-screen">
         {decorativeTheme ? <MatrixRain /> : null}
